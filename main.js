cca3abb4cc08b3634d36fccae1fdff27fc1d5795
federicoalagna <federicoalagna01@gmail.com>
2024-05-17 17:34:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// array con immagini e oggetti
+const immagini = [
+    {
+      src: '01.webp',
+      title: 'Miles Morales',
+      description: 'Descrizione 1'
+    },
+    {
+      src: '02.webp',
+      title: 'Ratchet',
+      description: 'Descrizione 2'
+    },
+    {
+      src: '03.webp',
+      title: 'Fortnite',
+      description: 'Descrizione 3'
+    },
+    {
+      src: '04.webp',
+      title: 'Stray',
+      description: 'Descrizione 4'
+    },
+    {
+      src: '05.webp',
+      title: 'Avengers',
+      description: 'Descrizione 5'
+    }
+  ];
+  
