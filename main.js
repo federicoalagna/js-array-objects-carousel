802f6629952a2b382fbdbd5ba549b47e81fb4cc2
federicoalagna <federicoalagna01@gmail.com>
2024-05-17 17:57:57 +0200

    creazione di una funzione

--- main.js
+++ main.js
@@ -28,4 +28,36 @@ const immagini = [
       description: 'Descrizione 5'
     }
   ];
+
+console.log(immagini)
   
+  // seleziono gli elementi HTML necessari
+const itemsContainer = document.querySelector('.items');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const titleElement = document.querySelector('.title');
+const descriptionElement = document.querySelector('.description');
+
+// immagine visualizzata
+let currentIndex = 0;
+
+// funzione per visualizzare un'immagine
+function displayImage(index) {
+    console.log('Displaying image at index:', index);
+
+    // estraggo i dati dell'immagine dall'array
+  const { src, title, description } = images[index];
+
+    // creo un elemento img 
+    const img = document.createElement('img');
+    img.src = src;
+    img.alt = title;
+
+    // svuoto il contenitore degli elementi e aggiungo l'immagine
+    itemsContainer.innerHTML = '';
+    itemsContainer.appendChild(img);
+
+    // aggionro il titolo e la descrizione
+    titleElement.textContent = title;
+    descriptionElement.textContent = description;
+}
